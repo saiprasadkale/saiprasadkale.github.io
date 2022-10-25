cfa3a9d3dc5a143c6405d8faa8ab4ef8e1d0bcbe
saiprasadkale <116162077+saiprasadkale@users.noreply.github.com>
2022-10-25 15:50:24 +0530

    Update App.js
    Change is good

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Miss Shavvi I love you a lot.</h1>
+      <h1>Hello site under construction .</h1>
     </div>
   );
 }
